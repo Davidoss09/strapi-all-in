58f18655cb33bc645ac1adfada3343b5b586c30e
happy_hunter <gherciu553@gmail.com>
2019-08-04 16:48:36 +0300

    initial structure

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+)
+
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier', 'react'],
+  env: {
+    commonjs: true,
+    jest: true,
+    browser: false,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: false,
+    },
+  },
+  globals: {
+    strapi: true
+  },
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+  },
+}
